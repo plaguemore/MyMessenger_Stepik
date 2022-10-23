a824b4aa7b64f20eaec780fc9980083ab4efde73
onfroy x <qwqlol.7696@gmail.com>
2022-10-23 21:43:41 +0300

    RestSharp

--- WindowsFormsClient/Form1.cs
+++ WindowsFormsClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyMessenger_Stepik;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,9 @@ namespace WindowsFormsClient
 {
   public partial class Form1 : Form
   {
+    private static int MessageID = 0;
+    private static string UserName;
+    private static MessangerClientAPI API = new MessangerClientAPI();
     public Form1()
     {
       InitializeComponent();
@@ -19,7 +23,32 @@ namespace WindowsFormsClient
 
     private void SendButton_Click(object sender, EventArgs e)
     {
-      UserNameTB.Text = MessagesTB.Text;
+      string UserName = UserNameTB.Text;
+      string Message = MessagesTB.Text;
+      if ((UserName.Length > 1) && (UserName.Length > 1))
+      {
+        MyMessenger_Stepik.Message msg = new MyMessenger_Stepik.Message(UserName, Message, DateTime.Now);
+        API.SendMessageRestSharp(msg);
+      }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      var getMessage = new Func<Task>(async () =>
+      {
+        MyMessenger_Stepik.Message msg = await API.GetMessageHTTPAsync(MessageID);
+        while (msg != null)
+        {
+          MessagesLB.Items.Add(msg);
+          MessageID++;
+          msg = await API.GetMessageHTTPAsync(MessageID);
+        }
+      });
+      getMessage.Invoke();
+    }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
     }
   }
 }
