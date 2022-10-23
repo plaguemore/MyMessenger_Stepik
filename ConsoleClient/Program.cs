ba76bc3e0d49dbbecbecc39f28eba2bc4f52a37b
onfroy x <qwqlol.7696@gmail.com>
2022-10-23 15:19:13 +0300

    Добавил класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,9 @@ namespace MyMessenger_Stepik
   {
     static void Main(string[] args)
     {
+      Message msg = new Message();
       Console.WriteLine("Начало проекта!");
+      Console.WriteLine(msg.ToString());
     }
   }
 }
